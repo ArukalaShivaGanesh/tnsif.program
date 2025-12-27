1b19013b595e0558d08e421c90aa58061798cc37
ArukalaShivaGanesh <shivaganesharukala4@gmail.com>
2025-12-27 17:24:57 +0530

    hf

---
+++ sriindu/src/com/tnsif/demo/day3/Acessmodifier.java
@@ -0,0 +1,15 @@
+package com.tnsif.demo.day3;
+
+public class Acessmodifier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s = new Student();
+		s.getsname("shiva");
+		s.getsid(109);
+		s.getsmarks(720);
+
+	}
+
+	}
+
---
+++ sriindu/src/com/tnsif/demo/day3/shiva.java
@@ -0,0 +1,15 @@
+package com.tnsif.demo.day3;
+import java.util.*;
+
+public class shiva {
+	shiva c=new shiva();
+	Scanner SC=new Scanner (System.in);
+	Scanner SC1=new  Scanner(System.in);
+	System.out.println("Enter your name");
+	String cname=SC.next();
+	System.out.println("Enter your salary");
+	int csal=SC.nextInt();
+	System.out.println("Enter your city");
+	String ccity=SC1.nextLine();
+	
+}
