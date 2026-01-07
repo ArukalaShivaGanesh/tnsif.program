92517e44d291a52c5abab1847c5129f22248f730
ArukalaShivaGanesh <shivaganesharukala4@gmail.com>
2026-01-07 18:35:25 +0530

    as

---
+++ sriindu/src/com/tnsif/demo/Assignment2/Typeconversionmain.java
@@ -0,0 +1,10 @@
+package com.tnsif.demo.Assignment2;
+
+public class Typeconversionmain {
+	public static void main(String[] args) {
+		Typeconversion obj=new Typeconversion();
+		obj.show();
+
+	}
+
+}
